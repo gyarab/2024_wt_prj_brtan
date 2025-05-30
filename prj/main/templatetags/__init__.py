e28ec2f63d3cda82a0b4c77592dbc8fed1b866ee
BrtanMatyas <matyas.brtan.s@gyarab.cz>
2025-05-30 08:41:26 +0200

    login

---
+++ prj/main/templatetags/__init__.py
@@ -0,0 +1,3 @@
+# templates/templatetags/__init__.py
+# This file is intentionally left empty to mark the directory as a Python package.
+# This allows Django to recognize the directory as a package for template tags.
