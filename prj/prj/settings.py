a649d5255d3c756fa8fb03ebce52e9b639924780
Matyáš Brtáň <matyas.brtan.s@gyarab.cz>
2025-05-30 12:10:45 +0200

    server

--- prj/prj/settings.py
+++ prj/prj/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-q_l$v00lk&8omx8!&nb%*8li8#do1f2u!lxwbjf=ds@7x5rmxo'
 DEBUG = True
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'your-production-domain.com']
+ALLOWED_HOSTS = ['*']
 
 # ----------------------------
 # APLIKACE
